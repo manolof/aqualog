7ec55191b3ec8894de88de303355c2340861c422
manolof <manolis09@gmail.com>
2018-11-15 18:15:21 +0100

    test(store): fix test of root reducer

--- src/reducers/root.test.js
+++ src/reducers/root.test.js
@@ -9,12 +9,12 @@ const store = createStore(rootReducer);
 describe('rootReducer', () => {
 	it(`should check that initial state of the root reducer matches
 		what child reducers return given an empty action`, () => {
-		expect(store.getState().authReducer).toEqual(authReducer(undefined, {}));
+		expect(store.getState().auth).toEqual(authReducer(undefined, {}));
 	});
 
 	it(`should check that child reducers handle an action`, () => {
 		const action = signInStartAction();
 		store.dispatch(action);
-		expect(store.getState().authReducer).toEqual(authReducer(undefined, action));
+		expect(store.getState().auth).toEqual(authReducer(undefined, action));
 	});
 });
